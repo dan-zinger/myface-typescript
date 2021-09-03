dc704b53d2cd7d61c6ea07ab1b553ce57b2d2b2c
Dan Zinger <Dan.Zinger@softwire.com>
2021-09-03 20:04:33 +0100

    user page almost done

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+    const button = document.getElementById("colorchange-button");
+    button.addEventListener('click', function() {
+        console.log("great success!!!")
+        const postlist = document.getElementsByClassName("eachpost");
+        for (var i = 0; i< postlist.length; i++) {
+            const post = postlist[i]
+            post.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById("change-background-color");
+    button.addEventListener('click', function() {
+        const body = document.getElementsByTagName("body")[0];
+        body.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+    });
 });
